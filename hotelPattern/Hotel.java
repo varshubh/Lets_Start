673dce4cd88a8b3e3eae5da8b378fd350cfc5aae
varshubh <shubham.1996.sharma@gmail.com>
2020-05-12 00:07:23 +0530

    Cleaning repo

--- hotelPattern/Hotel.java
+++
@@ -1,8 +0,0 @@
-package hotelPattern;
-
-public abstract class Hotel {
-	protected Cost regular=null;
-	protected Cost rewarded=null;
-	protected Integer rating=null;
-	public abstract int calculateBill(int weekDays, int weekEnds, String str);
-}
